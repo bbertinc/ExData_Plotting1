0f3a1bd73d55a06b20f1179dab2a68e141a88fa1
bbertinc <bbertincourt@gmail.com>
2015-04-10 16:07:50 -0400

    Code to produce plot1.png

---
+++ plot1.R
@@ -0,0 +1,15 @@
+
+## Read The Full Data into a Data Frame
+epcdata <- read.csv("./data/household_power_consumption.txt",sep=";",
+                    na.strings="?")
+
+## Extract data for 1st and 2nd of February 2007
+usedata <- subset(epcdata, Date == "1/2/2007" | Date == "2/2/2007")
+
+## Plot Histogram of Global Active Power
+with(usedata, hist(Global_active_power, col="red",main="Global Active Power",
+                   xlab="Global Active Power (kilowatts)"))
+
+## Save as a PNG File
+dev.copy(png,file="plot1.png",width = 480, height = 480, units = "px")
+dev.off()
